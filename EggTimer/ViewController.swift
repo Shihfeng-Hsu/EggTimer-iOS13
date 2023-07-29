f830f9254bfff3e45fb464d58bea7fa115e2ed45
Billy Hsu <ga017281@gmail.com>
2023-07-29 22:41:17 +0800

    switch function

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+  
+    
+    let eggTimes = [
+        "Soft":5, "Medium":7, "Hard":12
+    ]
+    
+    @IBAction func heardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+        switch hardness {
+        case "Soft" :
+            print(eggTimes["Soft"]!)
+        case "Medium" :
+            print(eggTimes["Medium"]!)
+        case "Hard" :
+            print(eggTimes["Hard"]!)
+        default:
+            print("Error")
 
-
+        }
+        
+    }
 }
